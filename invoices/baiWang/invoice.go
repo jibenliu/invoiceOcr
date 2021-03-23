3e31750ead3e84dc168a05923485d205e03189ef
jibenliu <liuzb@mingyuanyun.com>
2021-03-23 22:27:41 +0800

    添加代码

---
+++ invoices/baiWang/invoice.go
@@ -0,0 +1,19 @@
+package baiWang
+
+import "invoiceOcr/invoices"
+
+type Invoice struct {
+	invoices.InvoiceChannel
+}
+
+func (i Invoice) CheckParams(p *invoices.Context) error {
+	panic("implement me")
+}
+
+func (i Invoice) InvoiceByVoucher(p *invoices.Context) error {
+	panic("implement me")
+}
+
+func (i Invoice) Run(p *invoices.Context) {
+
+}
